6d7d40d7ce6874febf0d701f94f0835599213f61
eve-coldstone <jinhui.yu2019@hotmail.com>
2025-11-25 10:52:12 -0600

    Hash Map sol added

--- python/001_Two_Sum.py
+++ python/001_Two_Sum.py
@@ -1,5 +1,5 @@
-def twoSum(nums, target):
-    # two pointer
+# two pointer
+def twoSum_two_pointer(nums, target):
     nums_index = [(val, index) for index, val in enumerate(nums)]
     nums_index.sort()
     begin, end = 0, len(nums) - 1
@@ -12,9 +12,23 @@ def twoSum(nums, target):
         else:
             end -= 1
 
+# hash map
+def twoSum_hash_map(nums, target):
+    num_map = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], index]
+        num_map[num] = index
+
 if __name__ == "__main__":
     # Example usage
     nums = [2, 7, 11, 15]
     target = 9
-    result = twoSum(nums, target)
+    result = twoSum_two_pointer(nums, target)
+    print(f"-----Two Pointer Alg-----")
+    print(f"Indices of numbers that add up to {target}: {result}")
+
+    result = twoSum_hash_map(nums, target)
+    print(f"-----Hash Map Alg-----")
     print(f"Indices of numbers that add up to {target}: {result}")
