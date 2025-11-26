94f5e3a82c0226e3af13886662605a9d581c83c2
eve-coldstone <jinhui.yu2019@hotmail.com>
2025-11-26 09:37:29 -0600

    002, first ver

--- python/002_Add_Two_Numbers.py
+++ python/002_Add_Two_Numbers.py
@@ -1,57 +1,52 @@
+from timer_utils import measure_time
+
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-
-class Solution(object):
-    # def addTwoNumbers(self, l1, l2):
-    #     """
-    #     :type l1: ListNode
-    #     :type l2: ListNode
-    #     :rtype: ListNode
-    #     """
-    #     last = 0
-    #     head = prev = None
-    #     while True:
-    #         if l2 is None and l1 is None and last == 0:
-    #             break
-    #         val = last
-    #         if l2 is not None:
-    #             val += l2.val
-    #             l2 = l2.next
-    #         if l1 is not None:
-    #             val += l1.val
-    #             l1 = l1.next
-    #         if val >= 10:
-    #             val = val % 10
-    #             last = 1
-    #         else:
-    #             last = 0
-    #         current = ListNode(val)
-    #         if prev is None:
-    #             head = current
-    #         else:
-    #             prev.next = current
-    #         prev = current
-    #     return head
-
-    def addTwoNumbers(self, l1, l2):
+class Solution:
+    def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        curr = dummy
         carry = 0
-        # dummy head
-        head = curr = ListNode(0)
-        while l1 or l2:
-            val = carry
-            if l1:
-                val += l1.val
-                l1 = l1.next
-            if l2:
-                val += l2.val
-                l2 = l2.next
-            curr.next = ListNode(val % 10)
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+            curr.next = ListNode(total % 10)
+
             curr = curr.next
-            carry = int(val / 10)
-        if carry > 0:
-            curr.next = ListNode(carry)
-        return head.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        return dummy.next
+
+    def list_to_linked(lst):
+        dummy = ListNode()
+        curr = dummy
+        for val in lst:
+            curr.next = ListNode(val)
+            curr = curr.next
+        return dummy.next
+
+    def linked_to_list(node):
+        result = []
+        while node:
+            result.append(node.val)
+            node = node.next
+        return result
+
+    if __name__ == "__main__":
+        # Example usage
+        l1 = list_to_linked([2,4,3])
+        l2 = list_to_linked([5,6,4])
+        result = addTwoNumbers(l1, l2)
+        print(linked_to_list(result))
+        print(f"-----Brute Force Alg-----")
+        print(f"sum using linked list is {linked_to_list(l1)} + {linked_to_list(l2)} = {linked_to_list(result)}")
+        brute_force_time = measure_time(addTwoNumbers, l1, l2)
+        print(f"Brute Force avg time: {brute_force_time:.2f} μs")
